5d30cdda64e948411fec90a87856c3c1707b518f
Robert <bonjour_robert@outlook.com>
2020-07-28 23:23:53 +0200

    adds setData function

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,13 +1,18 @@
 var xhr = new XMLHttpRequest();
 
 xhr.open("GET",  "https://ci-swapi.herokuapp.com/api/");
-
 xhr.send();
 
+function setData(jsonData) {
+    data = jsonData;
+  console.log(data);
+}
 
 xhr.onreadystatechange = function() {
   if(this.readyState == 4 && this.status == 200) {
-        console.log(JSON.parse(this.responseText));
+        setData(JSON.parse(this.responseText));
+
+//    console.log(JSON.parse(this.responseText));
 //    document.getElementById('data').innerHTML = this.responseText;
   }
 };
