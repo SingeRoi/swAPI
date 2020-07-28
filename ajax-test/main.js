6353949c69db4b0c5cb8ce579cbc5094d876096f
Robert <bonjour_robert@outlook.com>
2020-07-28 23:46:39 +0200

    adds timeout function

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -3,16 +3,16 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET",  "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-  console.log(data);
-}
 
 xhr.onreadystatechange = function() {
+//  console.log(this.readyState);
   if(this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
 
 //    console.log(JSON.parse(this.responseText));
 //    document.getElementById('data').innerHTML = this.responseText;
   }
 };
+setTimeout(function() {
+  console.log(data);
+}, 500);
