8adab99122f8477014f614834d149cd16bd504ea
Robert <bonjour_robert@outlook.com>
2020-07-29 21:28:02 +0200

    returns ten objects

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -3,18 +3,29 @@ const baseURL = "https://ci-swapi.herokuapp.com/api/";
 function getData(type, cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", baseURL + type +"/"  );
+  xhr.open("GET", baseURL + type + "/");
   xhr.send();
 
   xhr.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
           cb(JSON.parse(this.responseText));
-
     }
   };
 }
-function writetodocument(type) {
-  getData(type, function(data) {
-      document.getElementById("data").innerHTML = data;
-  });
+//return to 1:23 in video 
+function writeToDocument(type) {
+    getData(type, function(data) {
+      console.dir(data);
+        document.getElementById("data").innerHTML = data.results;
+    });
 }
+
+/*function writeToDocument(type) {
+  getData(type, function(data) {
+      data = data.results;
+
+      data.forEach(function(item) {
+        document.getElementById("data").innerHTML += item.name;
+    });
+  }
+}*/
